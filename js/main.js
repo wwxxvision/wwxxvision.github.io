235608f45f49ada399f8d6ceafa3f624b7294062
Q-digital <no-reply@q-digital.org>
2020-02-04 17:36:46 +0400

    Added scrolling

--- js/main.js
+++ js/main.js
@@ -1,15 +1,89 @@
-const heightScreen = document.querySelector('.screen__portfolio').getBoundingClientRect().height;
-const startPointCurve = document.querySelector('.decor__circle').getBoundingClientRect().bottom;
-const curve = document.querySelector('.curve');
-const widthChooseBlock = document.querySelector('.screen__portfolio').getBoundingClientRect().width / 2;
+import AOS from 'aos';
 
-curve.style.height = heightScreen + 'px';
-curve.style.top = startPointCurve / 1.3   + 'px';
+console.log(AOS)
 
-window.addEventListener('scroll', function(e) {
+window.scrollTo(0, 0);
 
-  this.pageYOffset > 60  ? document.querySelector('.nav').classList.add('active') 
-    : document.querySelector('.nav').classList.remove('active')  
+function normalizeDecorCurve() {
+  const heightScreen = document.querySelector('.screen__portfolio').getBoundingClientRect().height;
+  const startPointCurve = document.querySelector('.decor__circle').getBoundingClientRect().bottom;
+  const curve = document.querySelector('.curve');
+
+  curve.style.height = heightScreen + 'px';
+  curve.style.top = startPointCurve / 1.3 + 'px';
+}
+
+window.addEventListener('scroll', function (e) {
+
+  this.pageYOffset > 60 ? document.querySelector('.nav').classList.add('active')
+    : document.querySelector('.nav').classList.remove('active')
 
 });
 
+
+//block for tabs
+
+function initTabs() {
+  const projectTabs = document.querySelectorAll('.list__item');
+  const allProjects = document.querySelectorAll('.project__page');
+
+  if (projectTabs.length) {
+    const clear = (allTabs) => allTabs.forEach((tab) => {
+      const textName = tab.childNodes[3];
+      tab.dataset.active = false;
+      tab.firstChild.nextSibling.classList.remove('select__active');
+      textName.classList.remove('tab__active');
+    });
+
+    const select = (tab) => {
+      allProjects.forEach((project) => project.dataset.active = false);
+
+      if (tab.dataset.active) {
+        const textName = tab.childNodes[3];
+
+        tab.firstChild.nextSibling.classList.add('select__active');
+        textName.classList.add('tab__active');
+
+        allProjects.forEach((project) => {
+          if (tab.dataset.project_id === project.dataset.project_id) {
+            project.dataset.active = true;
+            AOS.init();
+
+            let topPos = project.getBoundingClientRect().top + window.scrollY - 100;
+            console.log(project.getBoundingClientRect())
+            window.scroll({
+              top: topPos,
+              behavior: 'smooth'
+            });
+          }
+        });
+      }
+    }
+
+    projectTabs.forEach((tab, i) => {
+      tab.dataset.id = i;
+      tab.addEventListener('click', (e) => {
+        let _this = e.currentTarget;
+
+        clear(projectTabs);
+
+        _this.dataset.active = true;
+        select(tab);
+      });
+
+    });
+
+
+  }
+  else {
+    console.error('Undefined selector by list__item');
+  }
+}
+window.addEventListener('resize', (e) => {
+  initTabs();
+  normalizeDecorCurve();
+});
+
+
+initTabs();
+normalizeDecorCurve();
